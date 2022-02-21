3094f80a893d107e4a8e50ae333acd57bbbad331
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2022-02-21 19:39:40 -0300

    feature: creates design system for the app

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export enum Colors {
+  Primary = '#EB5528',
+  White = '#FFFFFF',
+  Black = '#000000',
+  Aqua = 'aqua',
+  Yellow = 'yellow',
+  DarkRed = '#991111',
+  Flame = '#DE541E',
+  Transparent = 'transparent',
+  MediumGrey = '#666666'
+}
